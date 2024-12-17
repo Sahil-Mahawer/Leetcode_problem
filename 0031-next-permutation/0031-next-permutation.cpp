edb5f9ba2033df6a6f01d83f6331fb27e8a222c5
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-17 12:42:30 +0530

    Time: 0 ms (100%), Space: 15.7 MB (21.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,8 +7,7 @@ public:
         int pivot = -1;
         int n = arr.size();
 
-        //find pivot element 
-
+        //find pivot element.. pivot element is element which is smaller than to its next element -- see from backward.. 
         for(int i=n-2;i>=0;i--){
             if(arr[i]<arr[i+1]){
                 pivot = i;
@@ -16,11 +15,13 @@ public:
             }
         }
 
+        // check if pivot exist or not .. if not exist return array in reverse order 
         if(pivot == -1){
             reverse(arr.begin(), arr.end());
             return;
         }
 
+        // now find element just greater than pivot .. it is always present at last..  because after pivot element , element are arranged in decreasing order.  and swap that element with pivot element 
         for(int i=n-1;i>pivot;i--){
             if(arr[i]>arr[pivot]){
                 swap(arr[i],arr[pivot]);
@@ -28,6 +29,7 @@ public:
             }
         }
 
+        //and at lat reverse array from pivot+1 element to n-1 element
         int i=pivot+1, j = n-1;
         while(i<=j){
             swap(arr[i], arr[j]);
