aa55117b5cd20e66dd46c609ab30638d0c3f48be
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-17 12:43:04 +0530

    Time: 0 ms (100%), Space: 15.7 MB (10.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     void nextPermutation(vector<int>& arr) {
         
-         next_permutation(arr.begin(),arr.end());
+        //  next_permutation(arr.begin(),arr.end());
         
-        // int pivot = -1;
-        // int n = arr.size();
+        int pivot = -1;
+        int n = arr.size();
 
-        // //find pivot element.. pivot element is element which is smaller than to its next element -- see from backward.. 
-        // for(int i=n-2;i>=0;i--){
-        //     if(arr[i]<arr[i+1]){
-        //         pivot = i;
-        //         break;
-        //     }
-        // }
+        //find pivot element.. pivot element is element which is smaller than to its next element -- see from backward.. 
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                pivot = i;
+                break;
+            }
+        }
 
-        // // check if pivot exist or not .. if not exist return array in reverse order 
-        // if(pivot == -1){
-        //     reverse(arr.begin(), arr.end());
-        //     return;
-        // }
+        // check if pivot exist or not .. if not exist return array in reverse order 
+        if(pivot == -1){
+            reverse(arr.begin(), arr.end());
+            return;
+        }
 
-        // // now find element just greater than pivot .. it is always present at last..  because after pivot element , element are arranged in decreasing order.  and swap that element with pivot element 
-        // for(int i=n-1;i>pivot;i--){
-        //     if(arr[i]>arr[pivot]){
-        //         swap(arr[i],arr[pivot]);
-        //         break;
-        //     }
-        // }
+        // now find element just greater than pivot .. it is always present at last..  because after pivot element , element are arranged in decreasing order.  and swap that element with pivot element 
+        for(int i=n-1;i>pivot;i--){
+            if(arr[i]>arr[pivot]){
+                swap(arr[i],arr[pivot]);
+                break;
+            }
+        }
 
-        // //and at last reverse array from pivot+1 element to n-1 element
-        // int i=pivot+1, j = n-1;
-        // while(i<=j){
-        //     swap(arr[i], arr[j]);
-        //     i++;
-        //     j--;
-        // }
+        //and at last reverse array from pivot+1 element to n-1 element
+        int i=pivot+1, j = n-1;
+        while(i<=j){
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
     }
 };
