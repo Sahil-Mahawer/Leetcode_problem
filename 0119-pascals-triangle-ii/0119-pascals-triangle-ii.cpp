86af63c13538fa27faeaf7f9124ce103dd1e2720
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-19 12:49:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (19.76%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     
-    int ncr(int n, int r){
-         long long  res =1;
+    // int ncr(int n, int r){
+    //      long long  res =1;
 
-         for(int i=0;i<r;i++){
-            res = res*(n-i);
-            res = res/(i+1);
-         }
-         return  (int)res;
-    }
+    //      for(int i=0;i<r;i++){
+    //         res = res*(n-i);
+    //         res = res/(i+1);
+    //      }
+    //      return  (int)res;
+    // }
     vector<int> getRow(int rowIndex) {
-        rowIndex = rowIndex+1;
-        // int i=0;
-        vector<int> ans(rowIndex);
-        for(int c=0;c<rowIndex;c++){
-            ans[c]= ncr(rowIndex-1 , c);
+        // rowIndex = rowIndex+1;
+        // // int i=0;
+        // vector<int> ans(rowIndex);
+        // for(int c=0;c<rowIndex;c++){
+        //     ans[c]= ncr(rowIndex-1 , c);
             
             
-        }
+        // }
         
-        return ans;
+        // return ans;
+        rowIndex = rowIndex+1;
+        long long ans =1;
+        vector<int>ansrow;
+        ansrow.push_back(1);
+
+        for(int col=1;col<rowIndex;col++){
+            ans = ans*(rowIndex-col);
+            ans = ans/col;
+            ansrow.push_back(ans);
+        }
+        return ansrow;
     }
+    
 
 };
