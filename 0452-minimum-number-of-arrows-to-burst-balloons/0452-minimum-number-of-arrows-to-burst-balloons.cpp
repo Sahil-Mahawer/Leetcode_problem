b8ef658380b18e978f5b597715c7c4a1a40fae00
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-20 18:18:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -3,10 +3,10 @@ public:
     int findMinArrowShots(vector<vector<int>>& points) {
         
          int n = points.size();
-         
-         sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
-         return a[1] < b[1];
-         });
+         sort(points.begin(), points.end());
+        //  sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+        //  return a[1] < b[1];
+        //  });
 
         if(n==0){
             return 0;
