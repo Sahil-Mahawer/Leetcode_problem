adfa71110e33c3b0d23e3c7e8d47ed2e5b50248f
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-24 21:48:16 +0530

    Time: 245 ms (48.21%), Space: 146.9 MB (35.95%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,16 +1,76 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        int n= nums.size();
 
-        for(int i=0;i<n-1;i++){
-            int min_index= i;
-            for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[min_index])
-                min_index=j;
-            }
-            swap(nums[min_index], nums[i]);
-        }
-           return nums;
+
+ void mergesort(vector<int>& nums , int start, int end){
+
+    if(start==end){
+        return;
+    }
+    int mid = start+(end-start)/2;
+
+    //left side
+    mergesort(nums, start , mid);
+
+    // right side
+    mergesort(nums, mid+1, end);
+
+    merge(nums, start, mid , end);
+
+ }
+ void  merge(vector<int>& nums, int start, int mid, int end){
+
+    vector<int> temp(end-start+1);
+
+    int left = start, right = mid+1, index=0;
+
+    while(left<=mid && right<=end){
+
+    if(nums[left]<=nums[right]){
+        temp[index] = nums[left];
+        index++;
+        left++;
+    }
+
+     else  //(nums[right]<=nums[left])
+     {
+        temp[index] = nums[right];
+      
+        index++;
+        right++;
+    }
+}
+
+    while(left<=mid){      // agrr left mai koii element bcha hai 
+        temp[index] = nums[left];
+        index++;
+        left++;
+    }
+
+    while(right<=end){      // agrr right mai koii element bcha hai 
+        temp[index] = nums[right];
+        index++;
+        right++;
+    }
+
+    index=0;
+    while(start<=end){
+        nums[start] = temp[index];
+        index++;
+        start++;
+    }
+
+
+  }
+
+    vector<int> sortArray(vector<int>& nums) {
+       
+        int n = nums.size();
+        int start = 0;
+        int end = nums.size()-1;
+
+        mergesort(nums,start,end);
+
+        return nums;
     }
 };
