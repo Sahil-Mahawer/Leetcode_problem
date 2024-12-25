7bbd7c0869e221371044062bd12dfe9fc64550ac
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2024-12-25 09:26:13 +0530

    Time: 79 ms (36.3%), Space: 53.8 MB (11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n= nums.size();
+    int subarraySum(vector<int>& arr, int k) {
+        // int n= nums.size();
         
-        int count=0;
+        // int count=0;
 
-        for(int i=0;i<n;i++){
-               int sum=0;
-            for(int j=i;j<n;j++){
-               sum=sum+nums[j];
-               if(sum==k){
-                count++;
-               }
-            }
-        }
-        return count;
+        // for(int i=0;i<n;i++){
+        //        int sum=0;
+        //     for(int j=i;j<n;j++){
+        //        sum=sum+nums[j];
+        //        if(sum==k){
+        //         count++;
+        //        }
+        //     }
+        // }
+        // return count;
+
+         int count=0;
+    int prefixSum=0;
+
+    map<int,int> mpp;
+
+    mpp[0]=1;
+
+    for(int i=0;i<arr.size();i++){
+        prefixSum+=arr[i];
+
+        int rem = prefixSum-k;
+
+        count+=mpp[rem];
+        mpp[prefixSum]+=1;
+    }
+
+    return count;
     }
 };
