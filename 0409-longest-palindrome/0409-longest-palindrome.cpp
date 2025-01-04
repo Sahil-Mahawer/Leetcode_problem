3b1117456a2c03eeb3c7c147929e3154a7c4f84d
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-01-04 19:57:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,29 +2,73 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         
-        unordered_map<char,int> freq;
+        // --->>> using hashmap --->>>> 
+
+        // unordered_map<char,int> freq;
+
+        // for(int i=0;i<s.size();i++){
+
+        //     freq[s[i]]++;
+        // }
+
+        // int count=0;
+        // bool odd=0;
+
+        // for(auto i=freq.begin(); i!=freq.end();i++){
+
+        //     int freq_count = i->second;
+
+        //     if(freq_count%2==0){
+        //         count += freq_count;
+        //     }
+
+        //     else
+        //     {
+        //         count += (freq_count-1);
+        //         odd=1;
+        //     }
+        // }
+
+        // return count+odd;
+
+        
+        // ------>>> using vector ----->>>>>
+
+        vector<int> lower(26,0) , upper(26,0);
 
         for(int i=0;i<s.size();i++){
 
-            freq[s[i]]++;
-        }
-
-        int count=0;
-        bool odd=0;
-
-        for(auto i=freq.begin(); i!=freq.end();i++){
-
-            int freq_count = i->second;
-
-            if(freq_count%2==0){
-                count += freq_count;
+            if(s[i]>='a'){  // lowercase character
+            lower[s[i]-'a']++;
             }
 
-            else
-            {
-                count += (freq_count-1);
+            else{  // uppercase alphabet
+              upper[s[i]-'A'];
+            }
+        }
+        int count = 0;
+        bool odd=0;
+        for(int i=0;i<26;i++){
+
+            if(lower[i]%2==0){
+                count += lower[i]; 
+            }
+
+            else{
+                count += (lower[i]-1);
                 odd=1;
             }
+
+
+            if(upper[i]%2==0){
+                count += upper[i]; 
+            }
+
+            else{
+                count += (upper[i]-1);
+                odd=1;
+            }
+
         }
 
         return count+odd;
