eeda2a52444689e627395e1ff38da38e13cfe2f0
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-01-08 23:58:47 +0530

    Time: 22 ms (24.43%), Space: 14.4 MB (28.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        int maxlength= 0;
+       
+    //    --->>>> Brute Force Approach ----->>>>
+
+    //     int maxlength= 0;
 
         int n = s.size();
 
-        for(int i=0;i<n;i++){
+    //     for(int i=0;i<n;i++){
 
-            unordered_set<char> unique;
+    //         unordered_set<char> unique;
 
-            for(int j=i;j<n;j++){
+    //         for(int j=i;j<n;j++){
 
-                if(unique.find(s[j])!=unique.end()){
-                    break;
-                }
+    //             if(unique.find(s[j])!=unique.end()){
+    //                 break;
+    //             }
 
-                unique.insert(s[j]);
-                maxlength = max(maxlength, j-i+1);
-            }
+    //             unique.insert(s[j]);
+    //             maxlength = max(maxlength, j-i+1);
+    //         }
+    //     }
+    //     return maxlength;
+
+
+    // ----->>>>> Optimal Approach ------>>>>
+
+    int start = 0, end = 0;
+    int maxlength=0;
+
+    unordered_set<char> unique;
+
+    while(end<n){
+
+        while(unique.find(s[end])!=unique.end()){
+
+            unique.erase(s[start]);
+            start++;
         }
-        return maxlength;
+        unique.insert(s[end]);
+        maxlength = max(maxlength, end-start+1);
+        end++;
     }
+
+    return maxlength;
+     }
 };
