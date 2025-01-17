0f0f81a083a3e2c3096c06cdac78b186b9447f68
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-01-18 00:20:44 +0530

    Time: 56 ms (28.91%), Space: 94.6 MB (27.02%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,78 +2,62 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         
-        // int n  = s.size();
-        // string sub ="";
-        // int size;
-
-        // for(int i=0;i<n-1;i++){
-        //     sub += s[i];
-        //     string check="";
-        //     if(n%2==0){
-
-        //         size = n / sub.size();
-
-        //         while(size){
-        //             check += s[i];
-        //             size--;
-        //         }
-
-        //         if(check==s){
-        //             return true;
-        //         }
-        //     }
-
-        //     else
-        //     {
-        //        size = n / sub.size();
-        //        while(size){
-        //             check += s[i];
-        //             size--;
-        //         }
-
-        //         if(check==s){
-        //             return true;
-        //         }
-        //     }
-
-        // }
-        // return false;
-
-         vector<int> lps(s.size(),0);
-        
-        int pre= 0;
-        int suff= 1;
-        
-        while(suff < s.size()){
+        int n  = s.size();
+       
+        for(int len=1;len<=n/2;len++){
             
-            if(s[pre] == s[suff]){
-                
-                lps[suff] = pre+1;
-                pre++;
-                suff++;
-                
-               
-                
-            }
-            
-            else
-            {
-                if(pre==0){
-                    lps[suff]=0;
-                    suff++;
+            if(n%len==0){
+
+                string sub = s.substr(0,len);
+                string check="";
+
+                for(int i=0;i<n/len;i++){
+                    check += sub;
                 }
-                
-                else
-                {
-                    pre = lps[pre-1];
+
+                if(check == s){
+                    return true;
                 }
             }
         }
-        
-       if(lps[lps.size()-1]==0){
+
         return false;
-       }
+
+    //      vector<int> lps(s.size(),0);
+        
+    //     int pre= 0;
+    //     int suff= 1;
+        
+    //     while(suff < s.size()){
+            
+    //         if(s[pre] == s[suff]){
+                
+    //             lps[suff] = pre+1;
+    //             pre++;
+    //             suff++;
+                
+               
+                
+    //         }
+            
+    //         else
+    //         {
+    //             if(pre==0){
+    //                 lps[suff]=0;
+    //                 suff++;
+    //             }
+                
+    //             else
+    //             {
+    //                 pre = lps[pre-1];
+    //             }
+    //         }
+    //     }
+        
+    //    if(lps[lps.size()-1]==0){
+    //     return false;
+    //    }
        
-       return true;
+    //    return true;
     }
 };
