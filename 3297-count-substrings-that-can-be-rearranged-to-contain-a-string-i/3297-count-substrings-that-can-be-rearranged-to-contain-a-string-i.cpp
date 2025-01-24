268ad2c009e7ce387eccefb43a256ea0e77768a7
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-01-24 17:22:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long validSubstringCount(string word1, string word2) {
+        unordered_map<char, int> freqMap, windowMap;
+        
+        // Count frequencies of characters in word2
+        for (char ch : word2) {
+            freqMap[ch]++;
+        }
+
+        int start = 0, required = freqMap.size(), current = 0;
+        long long count = 0;
+        int k = word2.length();
+
+        // Sliding window approach
+        for (int end = 0; end < word1.length(); end++) {
+            char curr = word1[end];
+
+            // If the character is present in word2, update the windowMap
+            if (freqMap.find(curr) != freqMap.end()) {
+                if (windowMap[curr] == freqMap[curr]) {
+                    current++;  // If the character count is less than required, increment current
+                }
+            }
+
+            windowMap[curr]++;
+
+            // When the window is valid, count all valid substrings
+            while (current == required) {
+                count += word1.length() - end;  // Add all valid substrings from start to end
+
+                // Shrink the window from the left
+                char pre = word1[start];
+                windowMap[pre]--;
+
+                // If the character no longer satisfies the frequency, decrement current
+                if (freqMap.find(pre) != freqMap.end()) {
+                    current--;
+                }
+
+                start++;
+            }
+        }
+
+        return count;
+    }
+};
