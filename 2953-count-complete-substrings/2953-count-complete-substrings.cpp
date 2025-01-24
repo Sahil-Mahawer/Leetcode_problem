fadca903c51f6758909a09becc0ad87a73b35708
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-01-24 10:50:07 +0530

    Time: 485 ms (26.99%), Space: 21.6 MB (46.01%) - LeetHub

--- 2953-count-complete-substrings/2953-count-complete-substrings.cpp
+++ 2953-count-complete-substrings/2953-count-complete-substrings.cpp
@@ -7,7 +7,7 @@ int solve(int start , int end , string &word , int k ){
 
     for(int unique_char = 1 ; unique_char <=26 && unique_char*k <= end-start+1 ;  unique_char++){
 
-        vector<int> count(26,0);
+        unordered_map<char,int> count;
         int goodboy=0;
 
         // Now start Sliding Window
@@ -18,12 +18,12 @@ int solve(int start , int end , string &word , int k ){
 
              char ch = word[j];
 
-             count[ch - 'a']++;
+             count[ch]++;
 
-             if(count[ch - 'a'] == k){
+             if(count[ch] == k){
                 goodboy+=1;
              }
-             else if(count[ch - 'a'] == k+1){
+             else if(count[ch] == k+1){
 
                 goodboy-=1;
              }
@@ -31,13 +31,13 @@ int solve(int start , int end , string &word , int k ){
             if(j-i+1 > windowlength){   // time to slide the window ... shrink from left side
 
                 // ith index character hatne vaala hai 
-                if(count[word[i] - 'a'] == k){
+                if(count[word[i]] == k){
                     goodboy-=1;
                 }
-                else if(count[word[i] - 'a'] == k+1){  // k+1-1 = k 
+                else if(count[word[i]] == k+1){  // k+1-1 = k 
                     goodboy+=1;
                 }
-                count[word[i] - 'a']--;
+                count[word[i]]--;
                 i++;
             }
 
@@ -64,7 +64,8 @@ int solve(int start , int end , string &word , int k ){
 
         for(int i=1;i<=n;i++){
 
-            if( i==n||abs(word[i]-word[i-1])>2){   // dividing condition 
+            if( i==n||abs(word[i]-word[i-1])>2){   // dividing condition  :- bORDERS DRAW krenge   
+            // i==n this for last segment .. only because of this condition we enter into the loop 
                 result +=solve(last , i-1, word , k);           // last ka matlab starting point 
                 last = i;
             }
