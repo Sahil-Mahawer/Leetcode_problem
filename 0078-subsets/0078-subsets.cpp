279d5ef1c1a7654362d232f5bcc9e885d427d670
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-02-11 18:47:57 +0530

    Time: 3 ms (34.15%), Space: 15.6 MB (19.06%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+void subsequence(vector<int> &nums , int index , int n , vector<vector<int>> &ans , vector<int> temp)
+{
+     if(index == n){
+        ans.push_back(temp);
+        return ;
+     }
+  // Not included
+  subsequence(nums , index+1, n , ans , temp);
+
+  // included
+  temp.push_back(nums[index]);
+   subsequence(nums , index+1, n , ans , temp);
+    
+}
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        vector<int> temp;
+        subsequence(nums , 0 , nums.size(), ans ,temp);
+        return ans;
+    }
+};
