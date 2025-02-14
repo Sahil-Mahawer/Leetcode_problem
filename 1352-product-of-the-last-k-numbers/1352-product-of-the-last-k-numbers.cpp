24fd27b18d53a79ffec0c3337917f248ab7d17c9
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-02-14 09:59:01 +0530

    Time: 20 ms (68.21%), Space: 77.9 MB (77.84%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,24 +1,52 @@
 class ProductOfNumbers {
 public:
-vector<int> store;
+// vector<int> store;  
+
+vector<int> cummulative_prod;  
+int n; 
+
     ProductOfNumbers() {
-        
+        n = 0;
     }
     
     void add(int num) {
-        store.push_back(num);
+        // store.push_back(num);
+
+        if(num ==0){
+            cummulative_prod = {};
+            n=0;
+        }
+        else{
+            if(cummulative_prod.empty()){ // empty hai
+                cummulative_prod.push_back(num);
+            }
+            else{ // empty nhi hai 
+                cummulative_prod.push_back(cummulative_prod[n-1]*num);
+            }
+            n++;
+        }
     }
     
     int getProduct(int k) {
-        int prod=1;
-        int i= store.size()-1;
-        while(k){
-            prod = prod*store[i];
-            i--;
-            k--;
+        // int prod=1;
+        // int i= store.size()-1;
+        // while(k){
+        //     prod = prod*store[i];
+        //     i--;
+        //     k--;
+        // }
+
+        // return prod;
+
+        if(n<k){
+            return 0;
         }
 
-        return prod;
+        else if(n==k){
+            return cummulative_prod[n-1];
+        }
+
+        return cummulative_prod[n-1]/cummulative_prod[n-k-1];
     }
 };
 
