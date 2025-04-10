0785181c3bcf1cb6355fff40bb362bb9bbbe99c6
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-04-10 17:39:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.85%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         
-        int count = 0;
-        stack<char> st;
+        int count = 0;              // Count unmatched closing parentheses ')'
+        stack<char> st;            // Stack to keep track of unmatched opening parentheses '('
 
-        for(int i=0;i<s.size();i++){
-
-            if(s[i] == '('){  // opening bracket
-                st.push(s[i]);
+        for(int i = 0; i < s.size(); i++) {   // Loop through the entire string
+            if(s[i] == '(') {     // If current character is an opening bracket
+                st.push(s[i]);    // Push it onto the stack
             }
-            else{  // closed bracket
-
-                if(st.empty()){
-                    count++;   // means you only have closed bracket no opening bracket is present 
+            else {                // If current character is a closing bracket ')'
+                if(st.empty()) {  // If there is no matching opening bracket
+                    count++;      // Increment count (we'll need to add an opening bracket)
                 }
-                else{
-                    st.pop();
+                else {
+                    st.pop();     // Pop the matching opening bracket from the stack
                 }
             }
         }
 
-        return count + st.size(); // st.size() is for like if only opening bracket is present   
+        // Final result is:
+        // count -> unmatched closing brackets
+        // st.size() -> unmatched opening brackets
+        return count + st.size();  // Total brackets to add to make string valid
     }
 };
