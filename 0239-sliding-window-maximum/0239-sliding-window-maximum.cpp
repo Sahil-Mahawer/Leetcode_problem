6017d4482908f0712e51a38ab1451e1c6d92b0b5
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-04-23 17:21:21 +0530

    Time: 17 ms (83.55%), Space: 139.1 MB (83.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,32 +2,72 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-       deque<int> dq;
+    //    deque<int> dq;
 
-       vector<int> result;
+    //    vector<int> result;
 
-       int n = nums.size();
+    //    int n = nums.size();
 
-       for(int i=0;i<n;i++){
+    //    for(int i=0;i<n;i++){
 
-        while(!dq.empty() && dq.front() <= i-k){  // window size cant be greater than k  
-            dq.pop_front();
+    //     while(!dq.empty() && dq.front() <= i-k){  // window size cant be greater than k  
+    //         dq.pop_front();
+    //     }
+
+    //     while(!dq.empty() && nums[i] > nums[dq.back()]){
+    //         dq.pop_back();
+    //     }
+
+    //     dq.push_back(i);
+
+    //     if(i>=(k-1)){
+    //         result.push_back(nums[dq.front()]);
+    //     }
+
+    //    }
+
+
+    //    return result;
+
+
+    deque<int> d;
+
+    vector<int> ans;
+
+    int n = nums.size();
+
+    for(int i=0;i<k-1;i++){
+
+        if(d.empty()){
+            d.push_back(i);
+        }
+        else{
+
+            while(!d.empty() && nums[i] > nums[d.back()]){
+
+                d.pop_back();
+
+            }
+            d.push_back(i);
+        }
+    }
+
+
+    for(int i = k-1; i<n; i++){
+
+        while(!d.empty() && nums[i] > nums[d.back()]){
+            d.pop_back();
         }
 
-        while(!dq.empty() && nums[i] > nums[dq.back()]){
-            dq.pop_back();
+        d.push_back(i);
+
+        while(d.front() <= i-k){  // check if element is in window size . if not then remove the element from start 
+            d.pop_front();
         }
 
-        dq.push_back(i);
-
-        if(i>=(k-1)){
-            result.push_back(nums[dq.front()]);
-        }
-
-       }
-
-
-       return result;
+        ans.push_back(nums[d.front()]);
+    }
         
+        return ans;
     }
 };
