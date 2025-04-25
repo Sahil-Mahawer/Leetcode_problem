6121edc090a6b34dc7a60de1f7abff82d170df8c
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-04-25 16:10:15 +0530

    Time: 179 ms (19.13%), Space: 117.9 MB (6.99%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         
-        priority_queue<pair<int,int>> maxHeap;
+        // priority_queue<pair<int,int>> maxHeap;
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+
+        multiset<int> st;
 
         int i = 0, j = 0;
 
@@ -14,19 +16,25 @@ public:
 
         while(j<n){
 
-            maxHeap.push({nums[j],j});
-            minHeap.push({nums[j],j});
+            // maxHeap.push({nums[j],j});
+            // minHeap.push({nums[j],j});
 
-            while(maxHeap.top().first - minHeap.top().first > limit){
-                 i = i+1;
+            st.insert(nums[j]);
 
-                 while(maxHeap.top().second < i){
-                    maxHeap.pop();
-                 }
+            // while(maxHeap.top().first - minHeap.top().first > limit){
+                while(*st.rbegin() - *st.begin() > limit){
+                //  i = i+1;  // min(maxHeap.top().second, minHeap.top().second) + 1;
 
-                while(minHeap.top().second < i){
-                    minHeap.pop();
-                 }  
+                //  while(maxHeap.top().second < i){
+                //     maxHeap.pop();
+                //  }
+
+                // while(minHeap.top().second < i){
+                //     minHeap.pop();
+                //  }  
+
+                st.erase(st.find(nums[i]));
+                i++;
       
             }
             max_length = max(max_length, j-i+1);
