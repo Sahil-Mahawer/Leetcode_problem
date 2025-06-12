734df6ac9902e6824f90f76bc3e9883e8c52ab6a
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-06-12 10:57:48 +0530

    Time: 4 ms (87.1%), Space: 35.6 MB (49.04%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -21,33 +21,63 @@ public:
 
         //----->>>> Optimal Approach ------->>>>>
 
-        int prefixSum=0;
-        int remainder, n=arr.size();
+        // int prefixSum=0;
+        // int remainder, n=arr.size();
 
-        int count=0;
+        // int count=0;
 
-        unordered_map<int,int> mpp; // store the remainder and its frequency
-        mpp[0]=1;
+        // unordered_map<int,int> mpp; // store the remainder and its frequency
+        // mpp[0]=1;
 
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
 
-            prefixSum+=arr[i];
-            remainder = prefixSum%k;
+        //     prefixSum+=arr[i];
+        //     remainder = prefixSum%k;
 
-            if(remainder<0){ // if remainder is negative then make it positive 
-                remainder = remainder+k; 
+        //     if(remainder<0){ // if remainder is negative then make it positive 
+        //         remainder = remainder+k; 
+        //     }
+
+        //     // similar logic as applied in "Subarray sum equals to K" .. if before see the reaminder then increase the count 
+
+        //     if(mpp.find(remainder)!=mpp.end()){ // checking remainder isse pehle aaya hai kya !! agr aaya hai toh jitni bhi baar aaya h utna count mai add krdo 
+        //         count = count+mpp[remainder];
+        //     }
+
+        //     mpp[remainder]++;  // and then increase the frequency of remainder.. 
+        // }
+
+        // return count;
+
+
+
+        unordered_map<int,int> mp;  // tp store the count of remainder
+
+        int prefix_sum = 0;
+
+        mp[0] = 1;
+
+        int total = 0;
+
+
+        for(int i=0; i<arr.size(); i++){
+
+            prefix_sum += arr[i];
+
+            int remainder = prefix_sum % k;
+
+            if(remainder < 0){
+                remainder = remainder + k; // to make remainder +ve
             }
 
-            // similar logic as applied in "Subarray sum equals to K" .. if before see the reaminder then increase the count 
-
-            if(mpp.find(remainder)!=mpp.end()){ // checking remainder isse pehle aaya hai kya !! agr aaya hai toh jitni bhi baar aaya h utna count mai add krdo 
-                count = count+mpp[remainder];
+            if(mp.find(remainder) != mp.end()){
+                total += mp[remainder];
             }
 
-            mpp[remainder]++;  // and then increase the frequency of remainder.. 
+            mp[remainder]++;
         }
 
-        return count;
+        return total;
 
     }
 };
