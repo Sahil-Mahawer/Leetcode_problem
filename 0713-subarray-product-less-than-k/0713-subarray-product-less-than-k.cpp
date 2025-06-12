8c630a25f6ccc374e1d7503bb776d44eb062ebd2
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-06-12 11:35:58 +0530

    Time: 2 ms (56.4%), Space: 65.2 MB (32.7%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,26 +1,81 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& arr, int k) {
-        int count=0;
-        if (k <= 1) return 0;
-        for(int i=0;i<arr.size();i++){
+
+         //----->>>>  Brute Force Approach --->>>>
+
+        // int count=0;
+        // if (k <= 1) return 0;
+
+        // for(int i=0;i<arr.size();i++){
             
-            int product = 1;
+        //     int product = 1;
             
 
-            for(int j=i;j<arr.size();j++){
+        //     for(int j=i;j<arr.size();j++){
 
-                product = product*arr[j];
+        //         product = product*arr[j];
 
-                if(product<k){
-                 count++;
-                }
-                else{
-                    break;
-                }
+        //         if(product<k){
+        //          count++;    // Count the current subarray as valid
+        //         }
+        //         else{
+        //             break;  // No point in continuing as the product will only grow larger
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+
+        // ----->>> Optimal Aprroach ------>>>
+
+
+
+        // int n = arr.size();
+
+        // int start=0;
+        // int end = 0;
+        // int product=1;
+        // int count=0;
+
+        // while(end<n){
+        //     product = product*arr[end];
+
+        //     while(product>=k && start<=end){
+        //         product = product/arr[start]; // shrinking phase
+        //         start++;
+        //     }
+        //     count = count+(end-start+1);
+        //     end++;
+        // }
+        // return count;
+
+
+
+        int product = 1;
+
+        int start = 0;
+        int end = 0;
+
+        int n = arr.size();
+
+        int count = 0;
+
+        while(end < n){
+
+            product = product * arr[end];
+
+            while(product >= k  && start <= end){
+
+                product = product / arr[start];
+                start++;
             }
+            count += end-start+1;
+            end++;
         }
-
+        
         return count;
+
     }
 };
