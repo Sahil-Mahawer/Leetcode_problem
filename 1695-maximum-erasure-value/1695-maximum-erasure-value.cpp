4edfdac3ff817c5a62053712ab57d51860e0a22e
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-07-22 20:56:51 +0530

    Time: 195 ms (33.89%), Space: 131 MB (43.04%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
         unordered_set<int> unique;
 
-        int start = 0, end =0;
+        int start = 0, end =0, maxsum = INT_MIN;
 
         while(end<n){
 
@@ -17,8 +17,9 @@ public:
             }
             unique.insert(nums[end]);
             sum +=nums[end];
+            maxsum = max(maxsum,sum); 
             end++; 
         }
-        return sum;
+        return maxsum;
     }
 };
