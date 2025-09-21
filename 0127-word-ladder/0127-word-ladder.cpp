29a89b5e01870772b694930f45247f59c9f6c017
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-09-21 13:33:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,46 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> word_set(wordList.begin(), wordList.end());
 
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        if (wordSet.find(endWord) == wordSet.end()) return 0;
+        queue<pair<string,int>> que;  // {words,steps)
 
-        queue<pair<string,int>> que; // {currentWord, steps}
-        que.push({beginWord, 1});
+        que.push({beginWord,1}); 
 
-        unordered_set<string> visited;
-        visited.insert(beginWord);
+        word_set.erase(beginWord); // and now start ereasing the words from set which you are getting because there is no point of getting back to that word 
 
-        while (!que.empty()) {
+        while(!que.empty()){
 
-           string word = que.front().first;
+            string word = que.front().first;
+            int steps = que.front().second;
 
-           int steps = que.front().second;
+            for(int i=0; i<word.size(); i++){
 
-           que.pop();
-
-            // Try changing each letter
-            for (int i = 0; i < word.size(); i++) {
                 string temp = word;
 
-                for (char c = 'a'; c <= 'z'; c++) {
-                    temp[i] = c;
+                for(char ch = 'a'; ch<='z'; ch++){
 
-                    if (temp == endWord) {
-                        return steps + 1;
+                    temp[i] = ch;
+
+                    if(temp == endWord){
+
+                        return steps+1;
+
                     }
 
-                    if (wordSet.count(temp) && !visited.count(temp)) {
-                        que.push({temp, steps + 1});
-                        visited.insert(temp);
+                    if(word_set.find(temp) != word_set.end()){
+
+                        que.push({temp, steps+1});
+
+                        word_set.erase(temp);
                     }
                 }
             }
         }
 
-        return 0; // No transformation possible
+
+        return 0;
+
     }
 };
