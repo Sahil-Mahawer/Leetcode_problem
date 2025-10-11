a47c46e964eade790dd7ff4c332c9201aaa4da85
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2025-10-11 10:51:47 +0530

    Time: 150 ms (24.09%), Space: 157.9 MB (10.23%) - LeetHub

---
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+int n;
+int K;
+
+int solve(vector<int>& energy, int i, vector<int>& t)
+{
+
+    if(i >=n){
+        return 0;
+    }
+
+    if(t[i] != INT_MIN){
+        return t[i];
+    }
+
+    return t[i] =  energy[i] + solve(energy, i+K, t);
+
+
+
+}
+    int maximumEnergy(vector<int>& energy, int k) {
+        
+        n = energy.size();
+        K = k;
+
+        vector<int> t(n, INT_MIN); 
+
+        int max_energy = INT_MIN;
+
+        for(int i=0; i<n; i++){
+
+            max_energy = max(max_energy, solve(energy,i, t));
+        }
+
+        return max_energy;
+    }
+};
