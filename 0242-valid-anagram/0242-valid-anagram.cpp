b3ed7490733cc250b34044de5f769147c686f0fe
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-03-10 22:38:18 +0530

    Time: 3 ms (47.05%), Space: 10.1 MB (10.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-       unordered_map<char,int> mpp1;
-       unordered_map<char,int> mpp2; 
+        unordered_map<char,int> mp1;
 
-       for(int i=0;i<s.size();i++){
-        mpp1[s[i]]++;
-       }
+        for(int i=0; i<s.size(); i++){
 
-       
-       for(int i=0;i<t.size();i++){
-        mpp2[t[i]]++;
-       }
+            mp1[s[i]]++;
+        }
 
-       if(mpp1 == mpp2 ){
-        return true;
-       }
-       
-       return false;
+        unordered_map<char,int> mp2;
 
+        for(int i=0; i<t.size(); i++){
+
+            mp2[t[i]]++;
+        }
+
+        if(mp1 == mp2){
+            return true;
+        }
+
+        return false;
     }
 };
