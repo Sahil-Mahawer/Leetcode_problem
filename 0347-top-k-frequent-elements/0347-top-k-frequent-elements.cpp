dba42db70e1feb09f7aba3850f15c4ef770b83e3
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-03-14 11:24:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,29 +3,26 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         vector<int> ans;
+        int n = nums.size();
 
         priority_queue<tuple<int,int>> st;
+        unordered_map<int,int> mp;
 
-        unordered_map<int,int> freq;
+        for(int i=0; i<n; i++){
 
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
+            mp[nums[i]]++;
         }
 
-        for(auto it = freq.begin(); it!= freq.end(); it++){
+        for(auto it=mp.begin(); it!=mp.end(); it++){
 
-            st.push({it->second, it->first});
+            st.push({it->second,it->first});
         }
 
-        while(k){
+        while(k--){
 
-            auto[f,v] = st.top();
-
-            ans.push_back(v);
-
+            auto[f,t] = st.top();
+            ans.push_back(t);
             st.pop();
-
-            k--;
         }
 
         return ans;
