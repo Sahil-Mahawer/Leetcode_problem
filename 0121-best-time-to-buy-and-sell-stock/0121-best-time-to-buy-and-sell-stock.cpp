0774e039ba460cdcdc8658a2aa6d3f1b6d4f6416
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-03-23 22:40:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,25 +3,38 @@ public:
     int maxProfit(vector<int>& prices) {
         
        int n = prices.size();
-       int profit = 0;
-       int max_profit = 0; 
-       vector<int> profitInFuture(n);
+    //    int profit = 0;
+    //    int max_profit = 0; 
+    //    vector<int> profitInFuture(n);
 
-       profitInFuture[n-1] = prices[n-1];
+    //    profitInFuture[n-1] = prices[n-1];
 
-       for(int i = n-2; i>=0; i--){
+    //    for(int i = n-2; i>=0; i--){
 
-        profitInFuture[i] = max(profitInFuture[i+1], prices[i]);
-       }
+    //     profitInFuture[i] = max(profitInFuture[i+1], prices[i]);
+    //    }
 
-       for(int i=0; i<n; i++){
+    //    for(int i=0; i<n; i++){
 
-        profit = profitInFuture[i] -  prices[i] ;
+    //     profit = profitInFuture[i] -  prices[i] ;
 
-        max_profit = max(max_profit, profit); 
-       }
+    //     max_profit = max(max_profit, profit); 
+    //    }
 
 
-        return max_profit;
+    //     return max_profit;
+
+
+    int mini = INT_MAX;
+    int max_profit = 0;
+
+    for(int i=0; i<n; i++){
+
+        mini = min(mini, prices[i]);
+
+        max_profit = max(max_profit, prices[i] - mini);
+    }
+
+    return max_profit;
     }
 };
