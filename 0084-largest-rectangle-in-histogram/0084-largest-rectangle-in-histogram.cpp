7a7510e7c879fe92400607d31361292b58be03a0
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-05-22 23:45:12 +0530

    Time: 31 ms (25.06%), Space: 89.5 MB (9.25%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         
-        int n = heights.size();
         stack<int> st;
 
-        vector<int> next_smallest_right(n);
-        vector<int> next_smallest_left(n);
+        int n = heights.size();
 
-        // next smallest right
-        for(int i=0;i<n;i++){
+        vector<int>next_smallest_left(n);
+        vector<int>next_smallest_right(n);
+
+        for(int i=0; i<n; i++){
 
             while(!st.empty() && heights[st.top()] > heights[i]){
-
                 next_smallest_right[st.top()] = i;
                 st.pop();
             }
-
             st.push(i);
         }
 
         while(!st.empty()){
-
-            next_smallest_right[st.top()] = n;   // jinka koi left smallest milla nhi unko 'n' dediya 
+            next_smallest_right[st.top()] = n;
             st.pop();
         }
 
 
-        // next smallest left
-        for(int i = n-1; i>=0; i--){
+        for(int i=n-1; i>=0; i--){
 
-            while(!st.empty() && heights[st.top()] > heights[i] ){
-                
+            while(!st.empty() && heights[st.top()] > heights[i]){
                 next_smallest_left[st.top()] = i;
                 st.pop();
             }
@@ -43,14 +38,13 @@ public:
             st.pop();
         }
 
-        int ans = 0;
+        int ans =0;
 
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n; i++){
 
-            ans = max(ans, heights[i]*(next_smallest_right[i] - next_smallest_left[i] - 1));
+            ans = max(ans, heights[i] * (next_smallest_right[i] - next_smallest_left[i] - 1));
         }
 
         return ans;
-
     }
 };
