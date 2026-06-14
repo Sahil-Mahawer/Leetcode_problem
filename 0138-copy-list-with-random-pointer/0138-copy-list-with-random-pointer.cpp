36b2edf47069b2cab44a6d28849e7d1886f082b2
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-06-14 18:00:59 +0530

    Time: 4 ms (68.32%), Space: 15.1 MB (46.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,102 +17,45 @@ public:
 class Solution {
 public:
 
-// Node * find(Node *curr1, Node* curr2, Node* x){
+Node *find(Node *curr1, Node *curr2, Node *x){
 
-//     while(curr1 != x){
-//         curr1 = curr1->next;
-//         curr2 = curr2->next;
-//     }
-//     return curr2;
-// }
+    if(x == NULL){
+        return NULL;
+    }
 
+    while(curr1 != x){
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+
+    return curr2;
+}
     Node* copyRandomList(Node* head) {
-
-         if (!head) return nullptr;
         
-        // Node *headcopy = new Node(0);
-        // Node *tailcopy = headcopy;
-
-        // Node *temp = head;
-
-        // // Clone Created without Random Pointer
-        // while(temp!=NULL){
-        //     tailcopy->next = new Node(temp->val);
-        //     tailcopy = tailcopy->next;
-        //     temp = temp->next;
-        // }
-        // tailcopy = headcopy;
-        // headcopy = headcopy->next;
-        // delete tailcopy;  // dummy node 0 deleted
-
-        // // Assign Random Pointer to Clone LL
-        // tailcopy = headcopy;
-        // temp = head;  // reset temp back to the head of original list
-
-        // while(temp!=NULL){
-        //     tailcopy->random = find(head, headcopy, temp->random);
-        //     temp = temp->next;
-        //     tailcopy = tailcopy->next;
-        // }  
-
-        // return headcopy;
-
-
-
-        Node *headcopy = new Node(0);
-        Node *tailcopy = headcopy;
+        Node* HeadCopy = new Node(0);
+        Node *TailCopy = HeadCopy;
 
         Node *temp = head;
 
         while(temp){
-            tailcopy->next = new Node(temp->val);
+            TailCopy->next = new Node(temp->val);
+            TailCopy = TailCopy->next;
             temp = temp->next;
-            tailcopy = tailcopy->next;
         }
 
-        tailcopy = headcopy;
-        headcopy = headcopy->next;
-        delete tailcopy;
+        TailCopy = HeadCopy;
+        HeadCopy = HeadCopy->next;
+        delete TailCopy;
 
-        tailcopy = headcopy;
+        TailCopy = HeadCopy;
+        temp = head;
 
-        Node *curr1 = head, *curr2 = headcopy;
-        Node *front1, *front2;
-
-        // inserting clone linkedlist into original linkedlist
-        while(curr1){
-            front1 = curr1->next;
-            front2 = curr2->next;
-
-            curr1->next = curr2;
-            curr2->next = front1;
-
-            curr1 = front1;
-            curr2 = front2;
-        }
-        curr1 = head;
-
-        // creating random pointer
-        while(curr1){
-
-            curr2 = curr1->next;
-
-            if(curr1->random){
-                curr2->random = curr1->random->next;
-            }
-
-            curr1 = curr2->next; // curr1 always point to original linkedlist 
+        while(temp){
+            TailCopy->random = find(head, HeadCopy, temp->random);
+            TailCopy = TailCopy->next;
+            temp = temp->next;
         }
 
-        // disconnect original and clone linkedlist with random pointer
-        curr1 = head;
-
-        while(curr1->next){
-            front1 = curr1->next;
-            curr1->next = front1->next;
-            curr1 = front1;
-        }
-
-        return headcopy;
+        return HeadCopy;
     }
 };
