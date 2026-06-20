ea32e4aa9c8a5737e9c92e523cbe34cb4a4c0514
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-06-20 18:53:22 +0530

    Time: 95 ms (36.8%), Space: 186.8 MB (9.81%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,125 +1,44 @@
-// Brute Force
-
-// class LRUCache {
-// public:
-
-// vector<pair<int,int>> cache;
-// int n;
-//     LRUCache(int capacity) {
-//         n = capacity;
-//     }
-    
-//     int get(int key) {
-        
-//         for(int i=0;i<cache.size();i++){
-
-//             if(cache[i].first == key){
-//                 int value = cache[i].second;
-
-//                 pair<int,int> temp = cache[i];
-
-//                 cache.erase(cache.begin() + i); // remove from that position  
-
-//                 cache.push_back(temp);  // store it at the back as it is most recently used
-
-//                 return value;
-//             }
-
-//         }
-        
-//         return -1;
-//     }
-    
-//     void put(int key, int value) {
-
-//         for(int i=0;i<cache.size();i++){
-
-//             if(cache[i].first == key){
-
-//                 cache.erase(cache.begin() + i);
-
-//                 cache.push_back({key,value});
-
-//                 return;
-//             }
-//         }
-
-//         if(cache.size() == n){
-
-//             cache.erase(cache.begin());
-
-//             cache.push_back({key, value});
-//         }
-
-//         else{
-
-//             cache.push_back({key, value});
-
-//         }
-
-        
-        
-//     }
-// };
-
-
 class LRUCache {
 public:
 
-// head->next = Most Recenlt used
-// tail -> prev = Least Recently used  
-
-// Here we have used Doubly Linked List
-
 class Node{
-    public:
+public:
+int key, val;
+Node *next, *prev;
 
-    int key, value;
-    Node *prev, *next;
+Node(int k, int v){
+    key = k;
+    val = v;
+    next = NULL;
+    prev = NULL;
 
-    Node(int k, int v){
-
-        key = k;
-        value = v;
-        prev = NULL;
-        next = NULL;
-
-    }
-
+}
 };
 
+    int limit;
+    unordered_map<int,Node*>mp;
 
+    void delNode(Node *Node_to_delete){
 
-int limit;
+        Node* prevOf_Node_to_delete  = Node_to_delete -> prev;
+        Node* nextOf_Node_to_delete = Node_to_delete -> next;
 
-unordered_map<int, Node*> mp;
+        prevOf_Node_to_delete ->next = nextOf_Node_to_delete;
+        nextOf_Node_to_delete -> prev =  prevOf_Node_to_delete;
+    }
 
-void delNode(Node * Node_To_delete){
+    void add_Node_after_Head(Node *newNode){
 
-    Node *prevOf_Node_toBe_deleted =  Node_To_delete->prev;
-    Node* nextOf__Node_toBe_deleted = Node_To_delete->next;
+        Node *nextOf_newNode = head->next;
+        head->next = newNode;
+        newNode->prev = head;
 
-    prevOf_Node_toBe_deleted -> next = nextOf__Node_toBe_deleted;
+        newNode->next = nextOf_newNode;
+        nextOf_newNode -> prev = newNode;
+    }
 
-    nextOf__Node_toBe_deleted -> prev = prevOf_Node_toBe_deleted;
-
-    
-}
-
-void addNode(Node *newNode){
-
-    Node * nextOf_newNode = head->next;
-
-    head->next = newNode;
-    newNode->prev = head;
-
-    newNode -> next = nextOf_newNode;
-    nextOf_newNode->prev = newNode;
-
-}
-
-Node *head = new Node(-1, -1);
-Node *tail = new Node(-1, -1);
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
 
     LRUCache(int capacity) {
         limit = capacity;
@@ -129,45 +48,39 @@ Node *tail = new Node(-1, -1);
     
     int get(int key) {
         
-        if(mp.find(key) == mp.end()){  // key not exist
-                return -1;
+        if(mp.find(key) == mp.end()){
+            return -1;
         }
 
         Node *ansNode = mp[key];
-        int ans = ansNode->value;
+        int ansValue = ansNode->val;
 
         mp.erase(key);
         delNode(ansNode);
 
-        addNode(ansNode);
+        add_Node_after_Head(ansNode);
         mp[key] = ansNode;
 
-
-        return ans;
-
+        return ansValue;
     }
     
     void put(int key, int value) {
         
         if(mp.find(key) != mp.end()){
 
-            Node *Node_To_delete = mp[key]; // node to be deleted
-            delNode(Node_To_delete);
+            Node *Node_to_delete = mp[key];
+            delNode(Node_to_delete);
             mp.erase(key);
-
         }
 
-        if(mp.size() == limit){  // if we reach to the limit now we delete the most least used cache 
-
+        if(mp.size() == limit){
             mp.erase(tail->prev->key);
             delNode(tail->prev);
-
         }
 
         Node *newNode = new Node(key, value);
-        addNode(newNode);
+        add_Node_after_Head(newNode);
         mp[key] = newNode;
-
     }
 };
 
