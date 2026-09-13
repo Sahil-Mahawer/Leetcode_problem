6dce38169557ce9eb23c3e9bc2b76e0741b6c4af
Sahil-Mahawer <140980955+Sahil-Mahawer@users.noreply.github.com>
2026-09-13 19:53:22 +0530

    Time: 76 ms (62.16%), Space: 112.8 MB (88.33%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+
+    int r;
+    int c;
+
+    int row[4] = {-1,1,0,0};
+    int col[4] = {0,0,-1,1};
+
+    bool isValid(int i, int j){
+
+        if(i>=0 && i<r && j>=0 && j<c){
+            return true;
+        }
+        return false;
+    }
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        
+        r = isWater.size();
+        c = isWater[0].size();
+
+        vector<vector<int>> result(r, vector<int>(c,-1));
+
+        queue<pair<int,int>> que;
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+
+                if(isWater[i][j] == 1){
+                    que.push({i,j});
+                    result[i][j] = 0;
+                }
+            }
+        }
+
+        while(!que.empty()){
+
+            int new_i = que.front().first;
+            int new_j = que.front().second;
+            que.pop();
+
+            for(int k=0; k<4; k++){
+
+                if(isValid(new_i + row[k], new_j + col[k])){
+
+                   if(result[new_i+row[k]][new_j+col[k]] == -1){
+                    result[new_i+row[k]][new_j+col[k]] = result[new_i][new_j] + 1;
+                    que.push({new_i+row[k], new_j+col[k]});
+                   }
+                }
+            }
+
+        }
+
+        return result;
+    }
+};
